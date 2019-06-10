5e25d3b5b0617be16ceeca19104f21abf7232d51
HW1 <harrison.white@gmail.com>
2019-06-10 11:59:36 +0100

    Correction made

--- weightconversion.py
+++ weightconversion.py
@@ -3,7 +3,7 @@ from tkinter import ttk
 
 def calculate(*args):
     try:
-        value = float(free.get())
+        value = float(feet.get())
         meters.set((0.3048 * value * 10000.0 + 0.5)/10000.0)
     except ValueError:
         pass
