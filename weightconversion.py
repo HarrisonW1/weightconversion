d228b6b8a598f7061c0a0a29448178e6a5e74dcd
HW1 <harrison.white@gmail.com>
2019-06-10 11:58:47 +0100

    defined

--- weightconversion.py
+++ weightconversion.py
@@ -1,6 +1,13 @@
 from tkinter import *
 from tkinter import ttk
 
+def calculate(*args):
+    try:
+        value = float(free.get())
+        meters.set((0.3048 * value * 10000.0 + 0.5)/10000.0)
+    except ValueError:
+        pass
+
 root = Tk()
 root.title("Feet to Meters")
 
